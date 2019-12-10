ded2027423ec1f854cb915daa9576d3bf63b2f44
765612 <765612@PCIN456823.cts.com>
2019-12-10 12:48:37 +0530

    d

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,9 +8,13 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek!";
-        System.out.println("Hi Harsha");
+    	
+    	
+    	
+    	System.out.println("Hi Harsha");
 		System.out.println("Hi Harsha");
+        return "Hello from GazGeek!";
+        
         
     }
 
